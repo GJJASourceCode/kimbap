be97c5180c64177e705aabb7024315b738a630ee
jindosz <127918186+jindosz@users.noreply.github.com>
2024-07-16 23:31:28 +0900

    final : update Intro

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public void SceneChangeCode()
+    public GameObject [] Intro;
+    int click = 0;
+    public void Start()
     {
-        SceneManager.LoadScene("Main");
+        for(int i = 0; i < 3; i++)
+        {
+            Intro[i].SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        if(click == 1)
+        {
+            Intro[2].SetActive(true);
+            Intro[0].SetActive(true);
+        }
+        else if(click == 2)
+        {
+            Intro[1].SetActive(true);
+        }
+        else if(click == 3)
+        {
+            SceneManager.LoadScene("Main");
+            click = 0;
+        }
+    }
+    
+    public void Click()
+    {   
+        click++;
     }
 }
